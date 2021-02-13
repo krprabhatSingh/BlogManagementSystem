8e970beba93b640aa0c5489f5e099dbce81e5f0f
prabhat <prabhatk287@gmail.com>
2021-02-14 00:52:12 +0530

    Django Administration

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post (models.Model):
@@ -10,12 +10,14 @@ class Post (models.Model):
                    )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
-                            unique_for_date='publish')
-    # author = models.ForeignKey(User,
-    #                            related_name='blog_posts')
+                            unique_for_date='publish', default=11)
+    """ on_delete we can use to fix the passing wrong number of argument issues"""
+    """ default we can use to fix non nullable field"""
+    author = models.ForeignKey(User,
+                               related_name='blog_posts', on_delete=models.CASCADE, default = 11)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(blank=True, null=True)
     updated = models.DateTimeField(auto_now=True)
     status  = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
